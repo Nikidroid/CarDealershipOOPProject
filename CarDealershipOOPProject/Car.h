94c57bdc5708121a9c764976cfa1e43060f01137
Krotovich Krot <for.games.straus3@gmail.com>
2024-03-28 13:11:17 +0300

    Added some constructors (default and canonical), initialized some cars with custom data type "Car.h".

---
+++ CarDealershipOOPProject/Car.h
@@ -0,0 +1,42 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Car {
+public:
+	string brand;
+	string model;
+	string body;
+	int year;
+	int price;
+
+
+	// Default constructor
+	Car() {
+		brand = "Indefinited brand";
+		model = "no name";
+		body = "unknown";
+		year = 0;
+		price = 0;
+
+	}
+
+	// Canonical constructor with parameters (arguments)
+	Car(string brnd, string mdl, string bd, int y, int p) {
+		brand = brnd;
+		model = mdl;
+		body = bd;
+		year = y;
+		price = p;
+	}
+
+	string convert() {
+		string s = "";
+		s += brand + " " + model;
+		s += ": body = " + body;
+		s += ", year of issue = " + to_string(year);
+		s += ", price = " + to_string(price) + " dollars.";
+		return s;
+	}
+};
--- CarDealershipOOPProject/Main.cpp
+++ CarDealershipOOPProject/Main.cpp
@@ -1,8 +1,22 @@
+#include "Car.h"
 #include <iostream>
 
 using namespace std;
 
 int main() {
+	Car car1("Toyota", "Supra","sport car", 2019, 90000);
+	Car car2("BMW", "M5 F90","sedan", 2017, 100000);
+	Car car3("Daewoo", "Matiz","micro car", 2015, 10000);
+	Car car4("Dodge", "RAM TRX","pickup", 2023, 115000);
+	Car car5("Lamborghini", "Aventador SVG","hyper car", 2019, 550000);
+	Car car6;
+
+	cout << car1.convert() << endl;
+	cout << car2.convert() << endl;
+	cout << car3.convert() << endl;
+	cout << car4.convert() << endl;
+	cout << car5.convert() << endl;
+	cout << car6.convert() << endl;
 
 	return 0;
 }
